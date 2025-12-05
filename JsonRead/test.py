b9ca8aa982ba2fbd8b8a1bdfc67925ee1b2b45f9
Carima-SSY <sysim@carima.co.kr>
2025-12-05 09:22:11 +0900

    Edit 1205

--- JsonRead/test.py
+++ JsonRead/test.py
@@ -1,21 +1,21 @@
 import json
 
-file_name = "DM400-1-1763618764.json"
-
+#file_name = "DM400-1-1763618764.json"
+file_name = "DM400-777777-1764818953.json"
 try:
 
     with open(file_name, 'r', encoding='utf-8') as f:
         data = json.load(f)
 
-
-    devdatas = data["data"]
-    for devdata in devdatas:
-        if devdata['device']['status'] == "PRINTING":
-            print("---------------------------------------------------------------------")
-            print(f"SENSOR: {devdata['sensor']}")
-            print(f"DEVICE: {devdata['device']}")
-            print(f"PRINT: {devdata['print']}")
-            print("---------------------------------------------------------------------")
+    print(data["preview-zip"])
+    # devdatas = data["data"]
+    # for devdata in devdatas:
+    #     if devdata['device']['status'] == "PRINTING":
+    #         print("---------------------------------------------------------------------")
+    #         print(f"SENSOR: {devdata['sensor']}")
+    #         print(f"DEVICE: {devdata['device']}")
+    #         print(f"PRINT: {devdata['print']}")
+    #         print("---------------------------------------------------------------------")
 except FileNotFoundError:
     print(f"오류: 파일 '{file_name}'을(를) 찾을 수 없습니다.")
 except json.JSONDecodeError:
